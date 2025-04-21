0da8c71b27178bab48212892245525445067b76a
junio <junio@DESKTOP-PVO2NIJ>
2025-04-21 10:01:27 -0400

    Adicionar arquivos de projeto.

---
+++ MoviesSita.Domain/Entities/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesSita.Domain.Entities
+{
+    public class Movies
+    {
+        public int id { get; set; }
+        public string title { get; set; } 
+    }
+}
---
+++ MoviesSita.Infra/Context/MoviesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesSita.Domain.Entities;
+namespace MoviesSita.Infra.Context
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions options) : base(options){}        
+        public DbSet<Movies> movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MoviesDbContext).Assembly);
+        }
+    }
+}
